179ebfbfa61d5ff2ae90e9c20de4710173f835f4
Keyd8n <160067115+Keyd8n@users.noreply.github.com>
2025-03-10 15:33:59 +0200

    Add files via upload

--- Lab1/index.php
+++ Lab1/index.php
@@ -5,7 +5,7 @@
 Варіант: №7
 Дата виконання: 10.03.2025
 
-Посилання на завдання 1(Виведення всіх чисел від 1 до 10): 
-Скріншот роботи завдання 1: 
-Посилання на завдання 2(Перевірка, чи є число (27) кратним 3): 
-Скріншот роботи завдання 2: 
+Посилання на завдання 1(Виведення поточної дати): https://github.com/Keyd8n/PHPLABS_Demchenko/blob/main/Lab1/zav1.php
+Скріншот роботи завдання 1: https://github.com/Keyd8n/PHPLABS_Demchenko/blob/main/Lab1/zav1.jpg
+Посилання на завдання 2(Перевірка, чи є рік (2024) високосним): https://github.com/Keyd8n/PHPLABS_Demchenko/blob/main/Lab1/zav2.jpg
+Скріншот роботи завдання 2: https://github.com/Keyd8n/PHPLABS_Demchenko/blob/main/Lab1/zav2.php
