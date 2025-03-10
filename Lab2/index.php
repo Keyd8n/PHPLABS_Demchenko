6263052b85d45a011810a221af2b0016d829e6d6
Keyd8n <160067115+Keyd8n@users.noreply.github.com>
2025-03-10 15:47:26 +0200

    Add files via upload

---
+++ Lab2/index.php
@@ -0,0 +1,11 @@
+Лабораторна робота: №2
+Тема: "Основи роботи з PHP"
+Виконав: Демченко Дмитро Володимирович
+Група: KN1-B22
+Варіант: №7
+Дата виконання: 10.03.2025
+
+Посилання на завдання 1(відстань між двома точками у просторі за формулою:): https://github.com/Keyd8n/PHPLABS_Demchenko/blob/main/Lab2/zav1.php
+Скріншот роботи завдання 1: https://github.com/Keyd8n/PHPLABS_Demchenko/blob/main/Lab2/zav1.jpg
+Посилання на завдання 2(демонструє використання цілого, дробового числа та їх явне перетворення): https://github.com/Keyd8n/PHPLABS_Demchenko/blob/main/Lab2/zav2.php
+Скріншот роботи завдання 2: https://github.com/Keyd8n/PHPLABS_Demchenko/blob/main/Lab2/zav2.jpg
