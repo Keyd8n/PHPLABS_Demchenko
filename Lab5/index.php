45514c40aa1aee7af1d07cc177d2702926eadf68
Keyd8n <kn1b22.demchenko@kpnu.edu.ua>
2025-03-18 14:00:54 +0200

    Update task references and screenshots in index.php for Lab 5

--- Lab5/index.php
+++ Lab5/index.php
@@ -5,7 +5,9 @@
 Варіант: №7
 Дата виконання: 20.03.2025
 
-Посилання на завдання 1(створення масиву із 10 випадкових чисел від 1 до 100. Виведіть максимальне та мінімальне значення.): https://github.com/Keyd8n/PHPLABS_Demchenko/blob/main/Lab4/zav1.php
-Скріншот роботи завдання 1: https://github.com/Keyd8n/PHPLABS_Demchenko/blob/main/Lab4/zav1.jpg
-Посилання на завдання 2(конвертує температуру з Цельсія у Фаренгейт.): https://github.com/Keyd8n/PHPLABS_Demchenko/blob/main/Lab4/zav2.php
-Скріншот роботи завдання 2: https://github.com/Keyd8n/PHPLABS_Demchenko/blob/main/Lab4/zav2.jpg
+Посилання на завдання 1(обчисліть факторіал числа поданого з форми (GET)): https://github.com/Keyd8n/PHPLABS_Demchenko/blob/main/Lab5/zav1.php
+Скріншот роботи завдання 1: https://github.com/Keyd8n/PHPLABS_Demchenko/blob/main/Lab5/zav1_1.jpg
+https://github.com/Keyd8n/PHPLABS_Demchenko/blob/main/Lab5/zav1_2.jpg
+Посилання на завдання 2(POST. Перевірте, чи текст містить хоча б одну велику літеру.): https://github.com/Keyd8n/PHPLABS_Demchenko/blob/main/Lab5/zav2.php
+Скріншот роботи завдання 2: https://github.com/Keyd8n/PHPLABS_Demchenko/blob/main/La5/zav2_1.jpg
+https://github.com/Keyd8n/PHPLABS_Demchenko/blob/main/La5/zav2_2.jpg
