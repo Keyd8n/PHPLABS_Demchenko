c2ac1927d98a7a28d34c08ac064124600de38ed1
Keyd8n <kn1b22.demchenko@kpnu.edu.ua>
2025-03-18 13:49:55 +0200

    new

---
+++ Lab5/index.php
@@ -0,0 +1,11 @@
+Лабораторна робота: №5
+Тема: "Робота з формами"
+Виконав: Демченко Дмитро Володимирович
+Група: KN1-B22
+Варіант: №7
+Дата виконання: 20.03.2025
+
+Посилання на завдання 1(створення масиву із 10 випадкових чисел від 1 до 100. Виведіть максимальне та мінімальне значення.): https://github.com/Keyd8n/PHPLABS_Demchenko/blob/main/Lab4/zav1.php
+Скріншот роботи завдання 1: https://github.com/Keyd8n/PHPLABS_Demchenko/blob/main/Lab4/zav1.jpg
+Посилання на завдання 2(конвертує температуру з Цельсія у Фаренгейт.): https://github.com/Keyd8n/PHPLABS_Demchenko/blob/main/Lab4/zav2.php
+Скріншот роботи завдання 2: https://github.com/Keyd8n/PHPLABS_Demchenko/blob/main/Lab4/zav2.jpg
